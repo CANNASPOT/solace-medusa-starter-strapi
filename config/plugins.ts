b6e818f95c2d14afd6dc8129277f1de450101de0
Mazuna <nicojuric@gmail.com>
2025-01-04 12:27:17 +0100

    Enhance S3 configuration in plugins.ts by adding explicit ACL settings and bucket parameters to improve access control management and ensure minio compatability.

--- config/plugins.ts
+++ config/plugins.ts
@@ -8,8 +8,13 @@ export default ({ env }) => ({
           secretAccessKey: env("DO_SPACE_SECRET_KEY"),
           endpoint: env("DO_SPACE_ENDPOINT"),
           region: env("DO_SPACE_REGION"),
+          // Add this line to explicitly disable ACLs
+          params: {
+            Bucket: env("DO_SPACE_BUCKET"),
+            ACL: null, // Or you might try 'private' if null doesn't work
+          },
         },
-        s3:{
+        s3: {
           bucket: env("DO_SPACE_BUCKET"),
           path: env("DO_SPACE_PATH"),
         },
